012cd3c633ae9c9b8425b09ad575b483522b11dd
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-14 23:53:21 +0900

    add array example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -10,11 +10,18 @@ fn main() {
     // println!("s3: {s3}");
 
     // タプル
-    let mut t = (1, "2");
-    let first = t.0;
-    let second = t.1;
-    t.0 = 2;
-    t.1 = "3";
-    println!("first: {first}");   // -> 1
-    println!("second: {second}"); // -> 2
+    // let mut t = (1, "2");
+    // let first = t.0;
+    // let second = t.1;
+    // t.0 = 2;
+    // t.1 = "3";
+    // println!("first: {first}");   // -> 1
+    // println!("second: {second}"); // -> 2
+
+    // 配列
+    let mut a: [i32; 3] = [0, 1, 2];
+    let b: [i32; 3] = [0; 3];
+    a[1] = b[1];
+    // a[2] = b[2];
+    println!("{:?}", &a[1..3]);
 }
