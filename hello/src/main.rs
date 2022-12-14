7bcb78724370bf8a89f27f580dbe23e002e0662b
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-14 23:45:21 +0900

    experiment reference

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,9 +1,10 @@
 fn main() {
     let s1: String = String::from("Hello, World!");
     let s2: &str = &s1;
-    let s3: String = s2.to_string();
-    println!("{s1}");
-    println!("{s2}");
-    println!("{s3}");
-    println!("Hello, world!");
+    // let mut s3: String = s2.to_string();
+    println!("s1: {s1}");
+    // s1 = "Changed".to_string();
+    println!("s1: {s1}");
+    println!("s2: {s2}");
+    // println!("s3: {s3}");
 }
