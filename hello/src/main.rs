e87d02fa4cd37947f6eed0317e5cee558e484b2d
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-14 23:38:42 +0900

    add string example

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -1,3 +1,9 @@
 fn main() {
+    let s1: String = String::from("Hello, World!");
+    let s2: &str = &s1;
+    let s3: String = s2.to_string();
+    println!("{s1}");
+    println!("{s2}");
+    println!("{s3}");
     println!("Hello, world!");
 }
