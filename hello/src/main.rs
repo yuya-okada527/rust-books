381ceadc9a71c6ffc580a4104bdabc526bfec7d2
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-16 22:31:23 +0900

    add all

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -242,12 +242,14 @@ fn main() {
     }
 
     impl Person {
-        fn say_name(&self) {
+        fn say_name(&self) -> &Self {
             println!("I am {}.", self.name);
+            self
         }
 
-        fn say_age(&self) {
+        fn say_age(&self) -> &Self {
             println!("I am {} year(s) old.", self.age);
+            self
         }
     }
 
@@ -255,6 +257,5 @@ fn main() {
         name: String::from("Taro"),
         age: 20,
     };
-    p.say_name();
-    p.say_age();
+    p.say_name().say_age();
 }
