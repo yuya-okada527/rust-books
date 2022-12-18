d366cfab89de7f66d3e07913ba3cbc808f227ff6
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-18 15:33:41 +0900

    add all

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -3,6 +3,7 @@
 // use std::io::Write;
 // use std::fmt::format;
 use std::thread;
+use std::sync::{Arc, Mutex};
 
 fn main() {
     // 文字列
@@ -441,13 +442,28 @@ fn main() {
     //     println!("Hello, world!");
     // });
     // dbg!(handle.join());
+    // let mut handles = Vec::new();
+    // for x in 0..10 {
+    //     handles.push(thread::spawn(move || {
+    //         println!("Hello, world!: {}", x);
+    //     }));
+    // }
+    // for handle in handles {
+    //     let _ = handle.join();
+    // }
+
+    // common memory
     let mut handles = Vec::new();
+    let data = Arc::new(Mutex::new(vec![1; 10]));
     for x in 0..10 {
+        let data_ref = data.clone();
         handles.push(thread::spawn(move || {
-            println!("Hello, world!: {}", x);
-        }));
+            let mut data = data_ref.lock().unwrap();
+            data[x] += 1;
+        }))
     }
     for handle in handles {
         let _ = handle.join();
     }
+    dbg!(data);
 }
