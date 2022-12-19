cd75b751d393978ecdd7252120bd8274ad37fdb9
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-19 23:39:11 +0900

    add all

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -469,11 +469,41 @@ fn main() {
     // dbg!(data);
 
     // messing passing
-    let (tx, rx) = mpsc::channel();
-    let handle = thread::spawn(move || {
-        let data = rx.recv().unwrap();
-        println!("{}", data);
-    });
-    let _ = tx.send("Hello, world!");
-    let _ = handle.join();
+    // let (tx, rx) = mpsc::channel();
+    // let handle = thread::spawn(move || {
+    //     let data = rx.recv().unwrap();
+    //     println!("{}", data);
+    // });
+    // let _ = tx.send("Hello, world!");
+    // let _ = handle.join();
+    let mut handles = Vec::new();
+    let mut data = vec![1; 10];
+    let mut snd_channels = Vec::new();
+    let mut rcv_channels = Vec::new();
+
+    for _ in 0..10 {
+        let (snd_tx, snd_rx) = mpsc::channel();
+        let (rcv_tx, rcv_rx) = mpsc::channel();
+        snd_channels.push(snd_tx);
+        rcv_channels.push(rcv_rx);
+
+        handles.push(thread::spawn(move || {
+            let mut data = snd_rx.recv().unwrap();
+            data += 1;
+            let _ = rcv_tx.send(data);
+        }));
+    }
+
+    for x in 0..10 {
+        let _ = snd_channels[x].send(data[x]);
+    }
+
+    for x in 0..10 {
+        data[x] = rcv_channels[x].recv().unwrap();
+    }
+
+    for handle in handles {
+        let _ = handle.join();
+    }
+    dbg!(data);
 }
