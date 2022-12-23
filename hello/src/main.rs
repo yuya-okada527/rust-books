51f1abf7d61b42877235770f5d056885f0bda281
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-23 21:46:19 +0900

    add all

--- hello/src/main.rs
+++ hello/src/main.rs
@@ -5,11 +5,11 @@
 // use std::thread;
 // use std::sync::{Arc, Mutex};
 // use std::sync::mpsc;
-use futures::{executor, future::join_all};
-use std::future::Future;
-use std::pin::Pin;
-use std::task::{Context, Poll};
-
+// use futures::{executor, future::join_all};
+// use std::future::Future;
+// use std::pin::Pin;
+// use std::task::{Context, Poll};
+use futures::executor;
 
 fn main() {
     // 文字列
@@ -513,26 +513,38 @@ fn main() {
     // dbg!(data);
 
     // Future
-    struct CountDown(u32);
+    // struct CountDown(u32);
 
-    impl Future for CountDown {
-        type Output = String;
-        fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<String> {
-            if self.0 == 0 {
-                Poll::Ready("Zero!!!".to_string())
-            } else {
-                println!("{}", self.0);
-                self.0 -= 1;
-                cx.waker().wake_by_ref();
-                Poll::Pending
-            }
-        }
+    // impl Future for CountDown {
+    //     type Output = String;
+    //     fn poll(mut self: Pin<&mut Self>, cx: &mut Context) -> Poll<String> {
+    //         if self.0 == 0 {
+    //             Poll::Ready("Zero!!!".to_string())
+    //         } else {
+    //             println!("{}", self.0);
+    //             self.0 -= 1;
+    //             cx.waker().wake_by_ref();
+    //             Poll::Pending
+    //         }
+    //     }
+    // }
+    // let countdown_future1 = CountDown(10);
+    // let countdown_future2 = CountDown(20);
+    // let cd_set = join_all(vec![countdown_future1, countdown_future2]);
+    // let res = executor::block_on(cd_set);
+    // for (i, s) in res.iter().enumerate() {
+    //     println!("{}: {}", i, s);
+    // }
+
+    // async await
+    async fn async_add(left: i32, right: i32) -> i32 {
+        left + right
     }
-    let countdown_future1 = CountDown(10);
-    let countdown_future2 = CountDown(20);
-    let cd_set = join_all(vec![countdown_future1, countdown_future2]);
-    let res = executor::block_on(cd_set);
-    for (i, s) in res.iter().enumerate() {
-        println!("{}: {}", i, s);
+
+    async fn something_greate_async_funtion() -> i32 {
+        let ans = async_add(2, 3).await;
+        println!("{}", ans);
+        ans
     }
+    executor::block_on(something_greate_async_funtion());
 }
