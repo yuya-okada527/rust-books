d95ae3b0868818e5fff83a58dec869f0bcffab43
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-25 14:13:26 +0900

    delete target

--- new_crate/target/debug/deps/new_crate-e77eaab7d8f799b8.d
+++
@@ -1,5 +0,0 @@
-/Users/okadayuya/Projects/github/books/rust/new_crate/target/debug/deps/new_crate-e77eaab7d8f799b8: src/main.rs
-
-/Users/okadayuya/Projects/github/books/rust/new_crate/target/debug/deps/new_crate-e77eaab7d8f799b8.d: src/main.rs
-
-src/main.rs:
