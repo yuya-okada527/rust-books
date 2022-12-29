eba6165c3417347cd2cf8ed0a5fb4143aef32828
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-29 23:08:51 +0900

    commit all

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -19,6 +19,18 @@ struct Opts {
     formula_file: Option<String>,
 }
 
+struct RpnCalculator(bool);
+
+impl RpnCalculator {
+    pub fn new(verbose: bool) -> Self {
+        Self(verbose)
+    }
+
+    pub fn eval(&self, formula: &str) -> i32 {
+        0
+    }
+}
+
 fn main() {
     let opts = Opts::parse();
 
@@ -34,8 +46,11 @@ fn main() {
 }
 
 fn run<R: BufRead>(reader: R, verbose: bool) {
+    let calc = RpnCalculator::new(verbose);
+
     for line in reader.lines() {
         let line = line.unwrap();
-        println!("{}", line);
+        let answer = calc.eval(&line);
+        println!("{}", answer);
     }
 }
