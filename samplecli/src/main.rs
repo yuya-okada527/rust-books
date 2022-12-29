8b1dfeaff435e43eae14647a5f6c3b559287c445
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-29 23:31:52 +0900

    commit all

--- samplecli/src/main.rs
+++ samplecli/src/main.rs
@@ -27,12 +27,43 @@ impl RpnCalculator {
     }
 
     pub fn eval(&self, formula: &str) -> i32 {
+        if self.0 {
+            println!("{}", formula);
+        }
         let mut tokens = formula.split_whitespace().rev().collect::<Vec<_>>();
         self.eval_inner(&mut tokens)
     }
 
     fn eval_inner(&self, tokens: &mut Vec<&str>) -> i32 {
-        0
+        let mut stack = Vec::new();
+
+        while let Some(token) = tokens.pop() {
+            if let Ok(x) = token.parse::<i32>() {
+                stack.push(x);
+            } else {
+                let y = stack.pop().expect("invalid syntax");
+                let x = stack.pop().expect("invalid syntax");
+                let res = match token {
+                    "+" => x + y,
+                    "-" => x - y,
+                    "*" => x * y,
+                    "/" => x / y,
+                    "%" => x % y,
+                    _ => panic!("invalid token"),
+                };
+                stack.push(res);
+            }
+
+            if self.0 {
+                println!("{:?} {:?}", tokens, stack);
+            }
+        }
+
+        if stack.len() == 1 {
+            stack[0]
+        } else {
+            panic!("invalid syntax")
+        }
     }
 }
 
