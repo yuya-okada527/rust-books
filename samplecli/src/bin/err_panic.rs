82d9f63b1500327415b267f42e74a59223a5943b
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-30 10:17:18 +0900

    commit all

--- samplecli/src/bin/err_panic.rs
+++ samplecli/src/bin/err_panic.rs
@@ -1,15 +1,17 @@
-fn get_int_from_file() -> i32 {
+fn get_int_from_file() -> Result<i32, String> {
     let path = "number.txt";
 
-    let num_str = std::fs::read_to_string(path).expect("failed to open the file.");
-    let ret = num_str
+    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
+    num_str
         .trim()
         .parse::<i32>()
-        .expect("failed to parse string to a number.");
-
-    ret * 2
+        .map(|t| t * 2)
+        .map_err(|e| e.to_string())
 }
 
 fn main() {
-    println!("{}", get_int_from_file());
+    match get_int_from_file() {
+        Ok(x) => println!("{}", x),
+        Err(e) => println!("{}", e),
+    }
 }
