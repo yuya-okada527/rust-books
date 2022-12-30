b28a8c760089561ba55ada56528326ac741906b3
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-30 10:24:41 +0900

    commit all

--- samplecli/src/bin/err_panic.rs
+++ samplecli/src/bin/err_panic.rs
@@ -1,17 +1,26 @@
-fn get_int_from_file() -> Result<i32, String> {
+enum MyError {
+    Io(std::io::Error),
+    Num(std::num::ParseIntError),
+}
+
+
+fn get_int_from_file() -> Result<i32, MyError> {
     let path = "number.txt";
 
-    let num_str = std::fs::read_to_string(path).map_err(|e| e.to_string())?;
+    let num_str = std::fs::read_to_string(path).map_err(|e| MyError::Io(e))?;
     num_str
         .trim()
         .parse::<i32>()
         .map(|t| t * 2)
-        .map_err(|e| e.to_string())
+        .map_err(|e| MyError::Num(e))
 }
 
 fn main() {
     match get_int_from_file() {
         Ok(x) => println!("{}", x),
-        Err(e) => println!("{}", e),
+        Err(e) => match e {
+            MyError::Io(cause) => println!("I/O Error: {}", cause),
+            MyError::Num(cause) => println!("Parse Error: {}", cause),
+        }
     }
 }
