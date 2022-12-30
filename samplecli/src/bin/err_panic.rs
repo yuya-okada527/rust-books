e00894069dcdf3e18b0c265f49a8aed4dabf0bd0
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-30 10:32:34 +0900

    commit all

--- samplecli/src/bin/err_panic.rs
+++ samplecli/src/bin/err_panic.rs
@@ -41,9 +41,6 @@ fn get_int_from_file() -> Result<i32, MyError> {
 fn main() {
     match get_int_from_file() {
         Ok(x) => println!("{}", x),
-        Err(e) => match e {
-            MyError::Io(cause) => println!("I/O Error: {}", cause),
-            MyError::Num(cause) => println!("Parse Error: {}", cause),
-        }
+        Err(e) => println!("{}", e),
     }
 }
