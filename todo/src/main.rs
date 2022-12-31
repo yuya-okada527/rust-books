bf7c5db9e61c4147d8dc080855f3a6d40235c079
yuya-okada527 <yuya.okada527@gmail.com>
2022-12-31 12:30:42 +0900

    commit all

--- todo/src/main.rs
+++ todo/src/main.rs
@@ -2,6 +2,10 @@ use actix_web::{get, App, HttpResponse, HttpServer, ResponseError};
 use thiserror::Error;
 use askama::Template;
 
+use r2d2::Pool;
+use r2d2_sqlite::SqliteConnectionManager;
+use rusqlite::params;
+
 struct TodoEntry {
     id: u32,
     text: String,
@@ -42,6 +46,20 @@ async fn index() -> Result<HttpResponse, MyError> {
 
 #[actix_rt::main]
 async fn main() -> Result<(), actix_web::Error> {
+    let manager = SqliteConnectionManager::file("todo.db");
+    let pool = Pool::new(manager).expect("Failed to initialize the connection pool.");
+    let conn = pool
+        .get()
+        .expect("Failed to get the connection from the pool.");
+    conn.execute(
+        "
+        CREATE TABLE IF NOT EXISTS todo (
+            id  INTEGER PRIMARY KEY AUTOINCREMENT,
+            text TEXT NOT NULL
+        )
+        ",
+        params![],
+    ).expect("Failed to create a table `todo`.");
     HttpServer::new(move || App::new().service(index))
         .bind("0.0.0.0:8080")?
         .run()
